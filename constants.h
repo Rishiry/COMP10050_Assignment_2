deb17ef5eed6e914d131341673f677e122ed1423
MuKhan03 <muhammad.khan1@ucdconnect.ie>
2022-04-02 16:46:00 +0000

    Add structs.h and constants.h

---
+++ constants.h
@@ -0,0 +1,22 @@
+#ifndef COMP10050_ASSIGNMENT_2_CONSTANTS_H
+#define COMP10050_ASSIGNMENT_2_CONSTANTS_H
+
+#endif
+
+//Max length of a tweet
+#define TWEET_LENGTH 280
+
+//Max length of a username
+#define USR_LENGHT 100
+
+//Max num of a followers
+#define MAX_FOLLOWERS 30
+
+//Max num of following users
+#define MAX_FOLLOWING 30
+
+//Max num of tweets in the news feed
+#define MAX_TWEETS 100
+
+//Max num of users
+#define MAX_USERS 100
---
+++ structs.h
@@ -0,0 +1,28 @@
+#ifndef COMP10050_ASSIGNMENT_2_STRUCTS_H
+#define COMP10050_ASSIGNMENT_2_STRUCTS_H
+
+#endif
+
+#include "constants.h"
+
+typedef struct tweet{
+    int id;
+    char msg[TWEET_LENGTH];
+    char user[USR_LENGHT];
+
+}tweet;
+
+typedef struct user{
+    char username[USR_LENGHT];
+
+    char followers[MAX_FOLLOWERS];
+    int num_followers;
+
+    char following[MAX_FOLLOWING];
+    int num_following;
+}user;
+
+typedef struct twitter{
+    struct user users[MAX_USERS];
+    struct tweet tweets[MAX_TWEETS];
+} twitter;
